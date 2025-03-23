752167f702afae968d0d5236c8475b4f28ecc778
delongfu <fdlhuanjingxinghe@outlook.com>
2025-03-23 23:48:26 +0800

    Update channel_model.py

--- models/channel_model.py
+++ models/channel_model.py
@@ -30,7 +30,7 @@ class InitSNRGenerator:
         if CONFIG.show.plot_hist_snr and self.show_cut:
             plt.subplot(1, 2, 1)
             # bins参数是指定bin(箱子)的个数，也就是总共有几条条状图
-            plt.hist(samples, bins = 13, edgecolor = 'black', alpha = 0.7)
+            plt.hist(samples, bins=13, edgecolor="black", alpha=0.7)
             plt.suptitle("SNR Distribution")
             plt.title("截断前")
         overflow_num = 0
@@ -47,29 +47,30 @@ class InitSNRGenerator:
 
         if CONFIG.show.plot_hist_snr and self.show_cut:
             plt.subplot(1, 2, 2)
-            plt.hist(cut_samples, bins = 13, edgecolor = 'black', alpha = 0.7)
+            plt.hist(cut_samples, bins=13, edgecolor="black", alpha=0.7)
             plt.title("截断后")
             if CONFIG.simulation.save_info:
                 import os
                 from datetime import datetime
+
                 # 如果存在results文件夹，则保存图片
-                if not os.path.exists('results'):
-                    os.makedirs('results')
+                if not os.path.exists("results"):
+                    os.makedirs("results")
                 # 创建当前时间子文件夹
-                timestamp = datetime.now().strftime('%Y%m%d_%H')
-                results_dir = os.path.join('results', timestamp)
+                timestamp = datetime.now().strftime("%Y%m%d_%H")
+                results_dir = os.path.join("results", timestamp)
                 if not os.path.exists(results_dir):
                     os.makedirs(results_dir)
-                plt.savefig(os.path.join(results_dir, 'SNR distribution.png'))
+                plt.savefig(os.path.join(results_dir, "SNR distribution.png"), dpi=300)
 
             plt.show()
 
         self.new_median = np.mean(cut_samples)
         return {
-            "values":        cut_samples,
+            "values": cut_samples,
             "overflow_rate": overflow_num / self.num_users,
-            "new_median":    self.new_median
-            }
+            "new_median": self.new_median,
+        }
 
 
 class AR_SNR_Generator:
@@ -91,23 +92,26 @@ class AR_SNR_Generator:
     def next_snr(self):
         """输出下一时刻SNR"""
         # 使SNR围绕初始值波动
-        self.current_snr = (self.alpha * self.current_snr + (1 - self.alpha) *
-                            self.init_snr_db + np.random.normal(0, self.sigma_ar))
-        if self.current_snr < CONFIG.channel.snr['init_range'][0]:
+        self.current_snr = (
+            self.alpha * self.current_snr
+            + (1 - self.alpha) * self.init_snr_db
+            + np.random.normal(0, self.sigma_ar)
+        )
+        if self.current_snr < CONFIG.channel.snr["init_range"][0]:
             self.current_snr = 0
-        elif self.current_snr > CONFIG.channel.snr['init_range'][1]:
-            self.current_snr = CONFIG.channel.snr['init_range'][1]
+        elif self.current_snr > CONFIG.channel.snr["init_range"][1]:
+            self.current_snr = CONFIG.channel.snr["init_range"][1]
         return round(self.current_snr, 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_snr_sequences = InitSNRGenerator(1000, 15, 4, 0, 25).generate()
     print(init_snr_sequences["new_median"])
 
     init_snr_gen = init_snr_sequences["values"][0]
     print("初始SNR:", init_snr_gen)
     # 使用示例
-    snr_gen = AR_SNR_Generator(init_snr_db = init_snr_gen, alpha = 0.9, sigma_db = 0.6)
+    snr_gen = AR_SNR_Generator(init_snr_db=init_snr_gen, alpha=0.9, sigma_db=0.6)
     print("两百点采样SNR序列 (dB):")
     lst = []
     for _ in range(10000):
